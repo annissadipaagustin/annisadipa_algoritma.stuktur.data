7e64b17b3d543a688b7e68b931567051dde602e4
annissadipaagustin <242310028@student.ibik.ac.id>
2024-12-24 12:36:14 +0700

    Add files via upload

---
+++ PERTEMUAN2/berat.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int berat = 48, tinggi = 147; 
+	cout << "Annissa Dipa" << endl;
+	cout <<"berat nisa: " <<berat << "kg";
+	cout <<" dan ";
+	cout <<"tinggi nisa: " <<tinggi << "cm" << endl; 
+	
+	return 0;
+}
