6f097aeb7e4aa8648f7c6f25cd02adc6f50786b6
annissadipaagustin <242310028@student.ibik.ac.id>
2024-12-24 12:34:19 +0700

    Add files via upload

---
+++ PERTEMUAN1/Nisa.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout <<"Hello World" <<endl;
+	cout <<"My name is Annisa Dipa Agustin " <<endl;
+	cout <<"you can call me Nisa" <<endl;
+	cout <<"I am 18th years old" <<endl;
+	cout <<"I live in Bogor City";
+	return 0;
+}
---
+++ PERTEMUAN1/tinggidanberat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int berat = 48, tinggi = 150; 
+	cout << "Annissa Dipa Agustin" << endl;
+	cout <<"berat Nisa: " <<berat << "kg" << endl; 
+	cout <<"tinggi Nisa: " <<tinggi << "cm" << endl; 
+	
+	return 0;
+}
